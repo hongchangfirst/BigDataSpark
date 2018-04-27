2c9753678ec91d96c6cbb5b7f9f22fbf4c8ad425
Zhang <hczhang@amazon.com>
2018-04-27 15:09:44 +0800

    Main Entrypoint

---
+++ src/main/java/com/zhc/demo/BigAnalysis.java
@@ -0,0 +1,30 @@
+package com.zhc.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import com.zhc.example.pagerank.PageRank;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+public class BigAnalysis {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("ZhcApp");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        PageRank.run(sc);
+    }
+
+
+}
